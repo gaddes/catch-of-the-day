e59f1c1f2502fe5d3b565dea67fb6f1295aaa511
Matthew Gaddes <matthew.gaddes@leighton.com>
2018-12-26 08:28:13 +0000

    Implement state.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  /*
+    This syntax (arrow function) allows us to access 'this' keyword from within function.
+    It's a more concise alternative to creating a constructor() method and binding 'this' within it.
+    See vid 11 - Events, Refs and this Binding.
+  */
+  goToStore = event => {
+    // 1. Stop the form from automatically submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    const storeName = this.myInput.current.value;
+    // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store -></button>
       </form>
     ) 
