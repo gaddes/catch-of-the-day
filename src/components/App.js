380091ec78394476ebb27d2c28f9243d651718ee
Matthew Gaddes <matthew.gaddes@leighton.com>
2018-12-27 07:43:57 +0000

    Persist state with Firebase.

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,26 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
   };
+
+  componentDidMount() {
+    const { params } = this.props.match;
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
   // We are passing this down to <Inventory> via props, see below!
   addFish = fish => {
     // 1. Take a copy of the existing state
